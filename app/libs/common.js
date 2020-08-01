7d4659916f0446da2b95978bc9eed0797a0870fe
Oleg <frontendercode@gmail.com>
2020-08-01 23:40:46 +0300

    v.0.26
    Настроена работа адаптивното меню

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,21 +1,47 @@
 $(document).ready(() => {
-  // вверхнее красиво-вращающееся меню
-  // 1 и 2 строка это анимация крестика
-  // 3 строка - слайдер вниз меню
-  // слайдер вниз меню отвечает за работу мобильного меню к переносу
-  $('.toggle-mnu').click(function () {
-    $(this).toggleClass('on');
-    $('.top-menu').slideToggle();
+  // $(() => {
+  //   function init() {
+  //     $('[data-behaviour="toggle-menu-icon"]').on('click', toggleMenuIcon);
+  //     $('[data-behaviour="toggle-link-icon"]').on('click', toggleSubMenu);
+  //     $('body, .nav ul li a').on('click', closeMenuIcon);
+  //   }
+
+
+  //   function closeMenuIcon() {
+  //     // $(this).removeClass('menu-icon--open');
+  //     $('[data-element="toggle-nav"]').removeClass('nav--active');
+  //   }
+
+
+  //   function toggleMenuIcon() {
+  //     $(this).toggleClass('menu-icon--open');
+  //     $('[data-element="toggle-nav"]').toggleClass('nav--active');
+  //   }
+
+  //   function toggleSubMenu() {
+  //     $(this).toggleClass('nav__link--plus nav__link--minus');
+  //     $('[data-behaviour="toggle-sub-menu"]').slideToggle('nav__sub-list--active');
+  //   }
+
+  //   init();
+  // });
+
+  // $('body, .nav ul li a').click(() => {
+  //   $('.nav').removeClass('nav--active');
+  //   // $('#toggle').removeClass('active');
+  //   // $('body').removeClass('stop');
+  // });
+
+
+  $('.menu-icon').click(() => {
+    $('.menu-icon').toggleClass('menu-icon--open');
+    $('[data-element="toggle-nav"]').toggleClass('nav--active');
+
     return false;
   });
-  $('body, .top-menu ul li a').click(() => {
-    $('.hidden-mnu').hide('slow');
-  });
 
-
-  $('.humburger').on('click', () => {
-    $('.humburger').toggleClass('humburger_active');
-    $('.menu__list').toggleClass('menu__list--active');
+  $('body, .overlay-menu ul li a').click(() => {
+    $('[data-element="toggle-nav"]').removeClass('nav--active');
   });
 
 
@@ -32,7 +58,7 @@ $(document).ready(() => {
   jQuery('#start_go').datetimepicker({ format: 'd.m.Y H:i' });
 
   /* Page Scroll to id fn call */
-  $('.top_line_menu ul li a, .menu ul li a').mPageScroll2id({
+  $('.top_line_menu ul li a, .nav ul li a').mPageScroll2id({
     layout: 'auto',
     offset: '.top_line',
     autoScrollSpeed: true,
